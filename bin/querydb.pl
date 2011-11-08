5eeaa7d3436bcd6fe6d6d884a01643a4402967b5
mokko <mauricemengel@gmail.com>
2011-11-07 20:07:26 -0500

    repair querydb.pl

--- bin/querydb.pl
+++ bin/querydb.pl
@@ -4,54 +4,66 @@
 
 use strict;
 use warnings;
+
+use FindBin;
 use DBD::SQLite;
-#encoding problem when dealing with data from sqlite
-use Encode qw(from_to decode);
+use Encode qw(from_to decode); #encoding problem when talking to sqlite
+use Cwd 'realpath';
+use Dancer ':syntax';
 
 use Getopt::Std;
-getopts( 'td', my $opts = {} );
+getopts( 'tdv', my $opts = {} );
 
 sub debug;
 
+=head1 SYNOPSIS
+
+querydb.pl -t '//xpath'
+
+//xpath is executed for every record for which has xml
+
+=head1 OPTIONS
+
+-t	truncated response, provide only identifier with matching xml
+
+-d  debug
+
+=cut
+
+
+
+Dancer::Config::setting( 'appdir', realpath("$FindBin::Bin/..") );
+Dancer::Config::load();
+config->{xpath}= 0;
+config->{debug}= 0;
+config->{truncate}= 0;
 
 #
-# config (todo)
+# getopt and @ARGV
 #
 
-my $config = {
-	xpath    => 0,
-	debug    => 0,
-	truncate => 0,
-
-	#TODO: dbfile should come from dancer's config
-	dbfile => '/home/Mengel/projects/Salsa_OAI2/data/db',
-};
-
-#
-# getopt
-#
+if ($opts->{v}) {
+	$opts->{d}=$opts->{v};
+}
 
 if ( $opts->{d} ) {
-	$config->{debug} = 1;
+	config->{debug} = 1;
 	debug "Debug mode on";
 
 }
 
 if ( $opts->{t} ) {
 	debug "Truncate on. Will show only identifiers which match";
-	$config->{truncate} = 1;
+	config->{truncate} = 1;
 }
-
-debug "Search for string in metadata: $ARGV[0]";
-
-#
-# command line options
-#
 
 if ( !$ARGV[0] ) {
 	print "Error: Query missing!\n";
 	exit 1;
 }
+
+debug "Search for string in metadata: $ARGV[0]";
+
 
 #this is, of course, a stupid error in the digester
 #and this is a bad work around, of course, but it is quick and works now
@@ -61,10 +73,10 @@ debug "encoding problem: $ARGV[0]";
 #
 # MAIN
 #
-my $dbh = DBI->connect( "dbi:SQLite:dbname=$config->{dbfile}", "", "" )
+my $dbh = DBI->connect( 'dbi:SQLite:dbname='.config->{dbfile}, "", "" )
   or die "Cannot connect to sqlite";
 
-if ( $config->{xpath} == 0 ) {
+if ( config->{xpath} == 0 ) {
 	fulltextSearch( $ARGV[0] );
 } else {
 	xpath( $ARGV[0] );
@@ -92,7 +104,7 @@ sub xpath {
 		print $aref->[0] . "\n";
 
 		#only print xml if truncate off
-		if ( $config->{truncate} == 0 ) {
+		if ( config->{truncate} == 0 ) {
 			$aref->[1] = decode( "utf8", $aref->[1] );
 			print $aref->[1] . "\n";
 		}
@@ -119,7 +131,7 @@ sub fulltextSearch {
 		print $aref->[0] . "\n";
 
 		#only print xml if truncate off
-		if ( $config->{truncate} == 0 ) {
+		if ( config->{truncate} == 0 ) {
 			$aref->[1] = decode( "utf8", $aref->[1] );
 			print $aref->[1] . "\n";
 		}
@@ -129,45 +141,11 @@ sub fulltextSearch {
 
 sub debug {
 	my $msg = shift;
-	if ( $config->{debug} > 0 ) {
+	if ( config->{debug} > 0 ) {
 		print $msg. "\n";
 	}
 }
 
-
+1;
 __END__
-=pod
-
-=head1 NAME
-
-querydb.pl - Query OAI data provider db directly using xpath1
-
-=head1 VERSION
-
-version 0.019
-
-=head1 SYNOPSIS
-
-querydb.pl -t '//xpath'
-
-//xpath is executed for every record for which has xml
-
-=head1 OPTIONS
-
--t	truncated response, provide only identifier with matching xml
-
--d  debug
-
-=head1 AUTHOR
-
-Maurice Mengel <mauricemengel@gmail.com>
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2011 by Maurice Mengel.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-
-=cut
 
